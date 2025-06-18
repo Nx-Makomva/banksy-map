8e79b0eac91c3b176373562946f211b60deb50e7
pat mikucka <mikuckapatrycja@gmail.com>
2025-06-18 15:21:45 +0100

    Badge tests done

--- api/tests/models/badge.test.js
+++ api/tests/models/badge.test.js
@@ -21,5 +21,58 @@ describe('Badge model', () => {
         expect(savedBadge.name).toEqual('1st LogIn');
     });
 
+    it('has a description', async () => {
+        const badge = new Badge({
+            name: '1st LogIn',
+            description: 'Welcome on board',
+            criteria: {
+                type: 'signup',
+                count: 0
+            }
+        });
+        const savedBadge = await badge.save();
+        expect(savedBadge.description).toEqual('Welcome on board');
+    });
 
-})
+    it('has correct criteria', async () => {
+        const badge = new Badge({
+            name: 'Wannabe explorer',
+            description: 'You have visited 3 art pieces',
+            criteria: {
+                type: 'visits',
+                count: 3
+            }
+        });
+        const savedBadge = await badge.save();
+        expect(savedBadge.criteria.type).toEqual('visits');
+        expect(savedBadge.criteria.count).toEqual(3)
+    });
+
+    it('is missing criteria count', async () => {
+        const badge = new Badge({
+            namme: 'Wannabe explorer',
+            description: 'You have visited 3 art pieces',
+            criteria: {
+                type: 'visits'
+            }
+        });
+        await expect(badge.save()).rejects.toThrow();
+    });
+
+    it('has invalid criteria type', async () => {
+        const badge = new Badge({
+            namme: 'Wannabe explorer',
+            description: 'You have visited 3 art pieces',
+            criteria: {
+                type: 'wrong',
+                count: 3
+            }
+        });
+        await expect(badge.save()).rejects.toThrow();
+    });
+
+    it('can list all badges', async () => {
+        const badges = await Badge.find();
+        expect(badges).toEqual([]);
+    });
+});
