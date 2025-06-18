ef537fece2c30115b5ed16dfe163e23a0e1d098e
Meg Atkinson <megan.e.atkinson@gmail.com>
2025-06-18 16:57:09 +0200

    basic tab implmentation in account

--- frontend/src/components/profile/ProfileMainContainer.jsx
+++ frontend/src/components/profile/ProfileMainContainer.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import { TabBar } from "./TabBar";
+import { CollectedContainer } from "./CollectedContainer";
+import { BookmarksContainer } from "./BookmarksContainer";
+import { CommentsContainer } from "./CommentsContainer";
+
+
 export function ProfileMainContainer() {
+    const [activeTab, setActiveTab] = useState('collected'); // 'collected', 'bookmarks' or 'comments'
+
+
     return (
         <>
         <div className="profileSideBar">
-        <p>Collected, Bookmarks, Comments</p>
+        <TabBar setActiveTab={setActiveTab}/>
+        {activeTab === 'collected' && <CollectedContainer/>}
+        {activeTab === 'bookmarks' && <BookmarksContainer/>}
+        {activeTab === 'comments' && <CommentsContainer/>}
         </div>
         </>
     );
