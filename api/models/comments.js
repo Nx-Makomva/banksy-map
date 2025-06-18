c1c92656fb5fd08d3f2a3563493e8b60d7574915
Nic <nicholasstm@gmail.com>
2025-06-18 15:09:19 +0100

    started comments tests

--- api/models/comments.js
+++ api/models/comments.js
@@ -4,7 +4,7 @@ const CommentsSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   artwork_id: { type: mongoose.Schema.ObjectId, ref: 'Artwork' },
   text: String,
-  createdAt: { type: DataTransfer, default: Date.now }
+  createdAt: { type: Date, default: Date.now }
 })
 
 const Comments = mongoose.model("Comment", CommentsSchema);
--- api/tests/models/comments.test.js
+++ api/tests/models/comments.test.js
@@ -5,5 +5,27 @@
 // create routes for artwork
 
 require('../mongodb_helper');
+const mongoose = require('mongoose');
 const Comment = require('../../models/comments');
+const User = require('../../models/user');
+const Artwork = require('../../models/artwork');
 
+// first check basic model construction
+// next test model relationship
+
+describe("Comments model constructs correctly", () => {
+  beforeEach(async () => {
+    await Comment.deleteMany({}) 
+  })
+  it("it creates a basic, valid comment", async () => {
+    const comment = await Comment.create({
+      user_id: new mongoose.Types.ObjectId(),
+      artwork_id: new mongoose.Types.ObjectId(),
+      text: 'I have fake ids'
+    });
+
+    expect(comment.text).toBe('I have fake ids');
+    expect(comment.createdAt).toBeDefined()
+  })
+  // END OF DESCRIBE //
+})
