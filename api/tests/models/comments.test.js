2c5a063262c2c6a4f789d664f339d0921849e216
Nic <nicholasstm@gmail.com>
2025-06-18 13:32:17 +0100

    updated tests for artworks model to match schema

---
+++ api/tests/models/comments.test.js
@@ -0,0 +1,9 @@
+// TODO: 
+// Write test files for models
+// write controller methods
+// write test files for controller methods
+// create routes for artwork
+
+require('../mongodb_helper');
+const Comment = require('../../models/comments');
+
