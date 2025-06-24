0b506d21b65d88d4c8de4d92161d54bcfdc15a09
Olly <ollywaldron11@yahoo.co.uk>
2025-06-24 20:37:43 +0100

    comments container, minor css

--- frontend/src/components/profile/ProfileMainContainer.jsx
+++ frontend/src/components/profile/ProfileMainContainer.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TabBar } from "./TabBar";
 import VisitsContainer  from "./VisitsContainer";
 import BookmarksContainer from "./BookmarksContainer";
-import { CommentsContainer } from "./CommentsContainer";
+import CommentsContainer from "./CommentsContainer";
 
 
 export function ProfileMainContainer({ setIsBookmarked, setIsVisited, onArtworkSelect }) {
@@ -15,7 +15,7 @@ export function ProfileMainContainer({ setIsBookmarked, setIsVisited, onArtworkS
         <TabBar setActiveTab={setActiveTab}/>
         {activeTab === 'collected' && <VisitsContainer setIsVisited={setIsVisited} onArtworkSelect={onArtworkSelect} />}
         {activeTab === 'bookmarks' && <BookmarksContainer setIsBookmarked={setIsBookmarked} onArtworkSelect={onArtworkSelect}/>}
-        {activeTab === 'comments' && <CommentsContainer/>}
+        {activeTab === 'comments' && <CommentsContainer onArtworkSelect={onArtworkSelect} />}
         </div>
         </>
     );
