a014ead9aa504b9587ca963bd290baea6aae83fb
Nic <nicholasstm@gmail.com>
2025-06-25 18:54:13 +0100

    increased timeout

--- api/jest.config.js
+++ api/jest.config.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./.env.test" });
 const config = {
   verbose: true, // Give more useful output
   maxWorkers: 1, // Make sure our tests run one after another
-  testTimeout: 20000
+  testTimeout: 30000
 };
 
 module.exports = config;
