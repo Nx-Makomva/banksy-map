2e465c0ef93a6cb2fca38af8e5ce48565ed62c05
Meg Atkinson <megan.e.atkinson@gmail.com>
2025-06-27 11:01:04 +0200

    some tidying - comments and extraneous files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,10 +51,9 @@ function App() {
     }
   };
 
-  // Check for existing session on app load
+  // Check for existing user session on app load
   useEffect(() => {
     getCurrentUser();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Function to refresh user data (can be called after login from other components)
